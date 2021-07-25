9ea397c964c4258857f2908cb40aa526c2327583
Joseph Salac <salacsoft22@gmail.com>
2021-07-25 18:16:06 +0800

    Create UserServiceTest

---
+++ tests/Feature/UserServiceTest.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class UserServiceTest extends TestCase
+{
+    /**
+     * @test
+     * @return void
+     */
+    public function it_can_return_a_paginated_list_of_users()
+    {
+	// Arrangements
+
+	// Actions
+
+	// Assertions
+    }
+
+    /**
+     * @test
+     * @return void
+     */
+    public function it_can_store_a_user_to_database()
+    {
+	// Arrangements
+
+	// Actions
+
+	// Assertions
+    }
+
+    /**
+     * @test
+     * @return void
+     */
+    public function it_can_find_and_return_an_existing_user()
+    {
+	// Arrangements
+
+	// Actions
+
+	// Assertions
+    }
+
+    /**
+     * @test
+     * @return void
+     */
+    public function it_can_update_an_existing_user()
+    {
+	// Arrangements
+
+	// Actions
+
+	// Assertions
+    }
+
+    /**
+     * @test
+     * @return void
+     */
+    public function it_can_soft_delete_an_existing_user()
+    {
+	// Arrangements
+
+	// Actions
+
+	// Assertions
+    }
+
+    /**
+     * @test
+     * @return void
+     */
+    public function it_can_return_a_paginated_list_of_trashed_users()
+    {
+	// Arrangements
+
+	// Actions
+
+	// Assertions
+    }
+
+    /**
+     * @test
+     * @return void
+     */
+    public function it_can_restore_a_soft_deleted_user()
+    {
+	// Arrangements
+
+	// Actions
+
+	// Assertions
+    }
+
+    /**
+     * @test
+     * @return void
+     */
+    public function it_can_permanently_delete_a_soft_deleted_user()
+    {
+	// Arrangements
+
+	// Actions
+
+	// Assertions
+    }
+
+    /**
+     * @test
+     * @return void
+     */
+    public function it_can_upload_photo()
+    {
+	// Arrangements
+
+	// Actions
+
+	// Assertions
+    }
+}
