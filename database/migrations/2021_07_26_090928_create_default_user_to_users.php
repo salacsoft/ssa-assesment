4b4bd166b19d5589329951817f67ccb9ee714777
Joseph Salac <salacsoft22@gmail.com>
2021-07-26 17:10:48 +0800

    move creation of default user

--- database/migrations/2021_07_26_090928_create_default_user_to_users.php
+++ database/migrations/2021_07_26_090928_create_default_user_to_users.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateDefaultUser extends Migration
+class CreateDefaultUserToUsers extends Migration
 {
     /**
      * Run the migrations.
@@ -25,6 +25,7 @@ class CreateDefaultUser extends Migration
             "photo" => "joseph.jpg",
             "password" => "secretkey"
         ]);
+
     }
 
     /**
